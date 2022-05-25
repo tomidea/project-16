a55a2c7800f19538f9716237ea7af39a59ee1d67
Babatomide Adedeji <51254648+tomidea@users.noreply.github.com>
2022-05-25 09:46:57 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
+        }
+
+provider "aws" {
+  region = var.region
+  shared_credentials_file = "~/.aws/credentials"
+  profile                  = "terraform-aws"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
+} 
+
+# Create public subnets1
+    resource "aws_subnet" "public" {
+    count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+    vpc_id                     = aws_vpc.main.id
+    cidr_block                 = cidrsubnet(var.vpc_cidr, 4 , count.index)
+    map_public_ip_on_launch    = true
+    availability_zone          = data.aws_availability_zones.available.names[count.index]
+
+}
